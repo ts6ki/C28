98808d3cbe504bd660b5d112c8868e8768b36972
ParadoxFlame <shivammaji5818@gmail.com>
2020-10-09 18:50:14 -0700

    updated 10/9/2020

--- sketch.js
+++ sketch.js
@@ -38,8 +38,12 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    var pointB ={
+        x: 100,
+        y: 100
+    }
+    chain = new SlingShot(bird.body,pointB);
 }
 
 function draw(){
@@ -63,6 +67,16 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
+    //log6.display();
     chain.display();    
+}
+
+function mouseDragged()
+{
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased()
+{ 
+    chain.fly();
 }
